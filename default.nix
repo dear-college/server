2b797214e208e4825218c85ec1501e03b3241493
Jim Fowler <fowler@math.osu.edu>
2024-05-20 21:38:30 -0400

    improvement to devops

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
 let
-  pkgs = import <nixpkgs> { };
   myHaskellPackages = pkgs.haskellPackages.override {
     overrides = self: super: {
+      dear-college = self.callCabal2nix "dear-college" ./. {};
       servant-auth-server = pkgs.haskell.lib.dontCheck (self.callHackage "servant-auth-server" "0.4.8.0" { });
     };
   };
 in
-myHaskellPackages.developPackage {
-  root = ./.;
-}
+myHaskellPackages.dear-college
+
